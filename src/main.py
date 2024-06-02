65c2fae04c4e5f8f9730538ee14a0c39ccb55f0b
gauravbole <gaurav.r.bole@gmail.com>
2024-06-02 17:18:27 +0530

    docker runnable image

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ app.include_router(users.router)
 app.include_router(memories.router)
 
 app.mount("/", StaticFiles(directory="web", html=True), name="web")
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True, workers=3)
+# docker build -t memor -f docker/Dockerfile .
+# docker run -d --name mycontainer7 -p 8000:8000 memor
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run('main:app', host="0.0.0.0", port=8000, workers=3)
