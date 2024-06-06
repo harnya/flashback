e5e4e9e5aeb1f1161d6631bcf488473802a86cec
gauravbole <gaurav.r.bole@gmail.com>
2024-06-06 22:39:16 +0530

    aws lambada image

--- src/main.py
+++ src/main.py
@@ -2,7 +2,21 @@ from fastapi import FastAPI
 from app.api.endpoints import users
 from app.api.endpoints import memories
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"],
+    
+)
+
+import mangum
 
 app.include_router(users.router)
 app.include_router(memories.router)
@@ -10,6 +24,8 @@ app.include_router(memories.router)
 app.mount("/", StaticFiles(directory="web", html=True), name="web")
 # docker build -t memor -f docker/Dockerfile .
 # docker run -d --name mycontainer7 -p 8000:8000 memor
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run('main:app', host="0.0.0.0", port=8000, workers=3)
+handler = mangum.Mangum(app)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run('main:app', host="0.0.0.0", port=8000)
