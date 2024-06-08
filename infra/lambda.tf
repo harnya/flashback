6bf88df533ccc59cfd031bfe1fde52e4814720c4
gauravbole <gaurav.r.bole@gmail.com>
2024-06-08 18:42:33 +0530

    api getway terraform script

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -25,3 +25,23 @@ resource "aws_lambda_function_url" "lambda_function_url" {
   }
 }
 
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowAPIgatewayInvokation"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.memory_lambda_tf.function_name
+  principal     = "apigateway.amazonaws.com"
+ 
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  # source_arn = "arn:aws:execute-api:${data.aws_region.current_region.name}:${var.aws_account_id}:${aws_api_gateway_rest_api.memoryapi.id}/*/*/"
+  source_arn = "arn:aws:execute-api:${data.aws_region.current_region.name}:${data.aws_caller_identity.current.account_id
+}:${aws_api_gateway_rest_api.memoryapi.id}/*/*/"
+}
+resource "aws_lambda_permission" "apigw_lambda_sub" {
+  statement_id  = "AllowAPIgatewayInvokationSub"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.memory_lambda_tf.function_name
+  principal     = "apigateway.amazonaws.com"
+ 
+  source_arn = "arn:aws:execute-api:${data.aws_region.current_region.name}:${data.aws_caller_identity.current.account_id
+}:${aws_api_gateway_rest_api.memoryapi.id}/*/*/*"
+}
