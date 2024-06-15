c2f0fdc2cdbd46f4263f48826a6ebcf3c5425c48
harnya <166941785+harnya@users.noreply.github.com>
2024-06-15 16:24:51 +0530

    s3 upload (#13)

--- src/app/services/memory.py
+++ src/app/services/memory.py
@@ -1,15 +1,17 @@
 
 from app.repositories.memory import MemoryRepository
 from app.models.memory import Memory, MemoryDetails
+from app.services.s3 import S3Media
 from typing import List
-
 class MemoryService:
 
     def __init__(self, memory_repo: MemoryRepository):
         self.memory_repo = memory_repo
 
     def add_memory(self, memory: Memory, memory_file: bytes | None) -> Memory:
-        memory_url = ""
+        s3_media = S3Media()
+
+        memory_url = s3_media.upload_file(file=memory_file)
         if not memory_file:
             memory_url = "https://avatars.githubusercontent.com/u/21107732?v=4"
         memory.memory_url = memory_url
